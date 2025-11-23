1abc72ae8f0fa0034f89e48deaac0e9ed594fd31
tomafouzder <tomafouzder12@gmail.com>
2025-11-24 03:01:36 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.POST || 3000;
+
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = "mongodb+srv://ecoTrack:l0vcAA5r9PSRbTvV@cluster0.ioz0rr9.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+
+        // Database
+        const database = client.db('ecoTrack');
+        const addNewCollection = database.collection('add_new_challenges')
+
+        app.get('/get-challenges', async (req, res) => {
+            const result = await addNewCollection.find().toArray();
+            res.send(result);
+        })
+
+
+        app.post('/add-challenges', async (req, res) => {
+            const data = req.body;
+            console.log(data);
+            const result = await addNewCollection.insertOne(data )
+            res.send({
+                success: true,
+                result
+            })
+        })
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+app.get("/", (req, res) => {
+    res.send("eco track app server is running")
+})
+
+
+app.listen(port, () => {
+    console.log(`eco track app server is running on port: ${port}`)
+})
