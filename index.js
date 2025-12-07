6c679e08255977f0cc7490b916962c7423a29ddf
tomafouzder <tomafouzder12@gmail.com>
2025-12-08 01:51:43 +0600

    completed

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ const verifyToken = async (req, res, next) => {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
 
         // Database
@@ -296,7 +296,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
 
